6f5e5b1e70174a2b0eb1136e0acab25ed91b6d57
galenomoon <gui.galenocover@gmail.com>
2022-05-23 11:48:27 -0300

    first commit learing react-native

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
+  const name = "galeno"
+  const img = "https://i.scdn.co/image/ab6761610000e5eb9ecfcee83a3e86d021033107"
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Galeno name={name} img={img} />
+    </View>
+  );
+}
+
+export function Galeno({ name, img }) {
+  return (
+    <View style={styles.container}>
+      <Text style={{ color: "red" }}>Oi!</Text>
       <StatusBar style="auto" />
+      <Image
+        style={styles.picture}
+        source={{
+          uri: img
+        }} />
+      <Text>
+        {name}
+      </Text>
     </View>
   );
 }
@@ -16,5 +34,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  picture: {
+    width: 250,
+    height: 250,
   },
 });
